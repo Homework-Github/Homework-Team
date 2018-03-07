cffc7a83af44f2cd0c8f0d6d023a45b4e19a2914
W0lfeR <W0lfeRRecordings@gmail.com>
2018-03-07 17:21:30 +0200

    Add new folder

---
+++ 1CSharpIntroAndBasicSyntax/Problem1Greeting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem1Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
